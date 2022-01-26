7ff29b184257ce16afbdb6af920f332cf1a614ea
blindLightning <blind.lightning.productions@gmail.com>
2022-01-26 18:44:04 +0000

    adds the starts of a login system, however the input dialog isn't displaying yet

--- libs/login.py
+++ libs/login.py
@@ -1,2 +1,11 @@
 from .globals import *
 #this is where the functions for logging into AO3 are stored. We will call these from the main function in libs/main.py. 
+
+def login():
+	login={"user":"","password":""}
+	dlg=wx.TextEntryDialog(frame, 'So you can brouse Archive of Our Own, please enter your AO3 username. ','login')
+	dlg.SetValue("")
+	if dlg.ShowModal() == wx.ID_OK:
+		login['user']=dlg.GetValue()
+	dlg.Destroy()
+	
--- libs/main.py
+++ libs/main.py
@@ -2,4 +2,4 @@ from .globals import *
 #the file where the bulk of the action occurs
 
 def main():
-	return
+	login.login()
