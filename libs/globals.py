c5e3dc0db47c117fb06d4868a6948496f2cc9685
blindLightning <blind.lightning.productions@gmail.com>
2022-01-26 17:38:30 +0000

    adds a basic toolbar to the main window. Next step is to add login system.

--- libs/globals.py
+++ libs/globals.py
@@ -2,3 +2,4 @@ import AO3
 import wx
 import cryptography
 from libs import login
+from libs import main
--- libs/main.py
+++ libs/main.py
@@ -1,2 +1,5 @@
 from .globals import *
 #the file where the bulk of the action occurs
+
+def main():
+	return
