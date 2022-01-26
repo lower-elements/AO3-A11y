f618d7a383f2ca8bb8249de559ac84b2250ff4ec
blindLightning <blind.lightning.productions@gmail.com>
2022-01-26 20:36:47 +0000

    adds login system, doesn't save for now however

--- libs/login.py
+++ libs/login.py
@@ -2,30 +2,30 @@ from . import globals as g
 #this is where the functions for logging into AO3 are stored. We will call these from the main function in libs/main.py. 
 
 def login():
-	try:
+	with open("configfiles/user.usr","r") as f:
+		user = f.read()
 		try:
-			with open('./configfiles/user.usr','r') as f:
-				user=f.read()
-			password=g.keyring.get_password('AO3',user)
-			g.session = g.AO3.Session(user,password)
-		except:
-			login={"user":"","password":""}
-			dlg=g.wx.TextEntryDialog(g.frame, 'Please enter your Archive of Our Own username so you can browse AO3.','login')
-			dlg.SetValue("")
-			if dlg.ShowModal() == g.wx.ID_OK:
-				login['user']=dlg.GetValue()
-			dlg.Destroy()
-			dlg=g.wx.TextEntryDialog(g.frame, 'Please enter your Archive of Our Own password so you can browse AO3.','login')
-			dlg.SetValue("")
-			if dlg.ShowModal() == g.wx.ID_OK:
-				login['password']=dlg.GetValue()
-			dlg.Destroy()
-			g.keyring.set_password('AO3', login['user'], login['password'])
-			with open('user.usr','w') as f:
-				f.write(login['user'])			
-			g.session = g.AO3.Session(login['user'],login['password'])
+			if user != "":
+				password=g.keyring.get_password('AO3',user)
+				g.session = g.AO3.Session(user,password)
+			
+			else:
+				login_dict={"user":"","password":""}
+				dlg=g.wx.TextEntryDialog(g.frame, 'Please enter your Archive of Our Own username so you can browse AO3.','login')
+				dlg.SetValue("")
+				if dlg.ShowModal() == g.wx.ID_OK:
+					login_dict['user']=dlg.GetValue()
+				dlg.Destroy()
+				dlg=g.wx.TextEntryDialog(g.frame, 'Please enter your Archive of Our Own password so you can browse AO3.','login')
+				dlg.SetValue("")
+				if dlg.ShowModal() == g.wx.ID_OK:
+					login_dict['password']=dlg.GetValue()
+				dlg.Destroy()
+				g.keyring.set_password('AO3', login_dict['user'], login_dict['password'])
+				with open('user.usr','w+') as f:
+					f.write(login_dict['user'])			
+				g.session = g.AO3.Session(login_dict['user'],login_dict['password'])
 		
-	except:
-		g.wx.MessageBox('Please try again and try checking if your username and password are correct', 'Warning', g.wx.OK | g.wx.ICON_WARNING)
-		g.frame.On_Quit()
+		except:
+			g.wx.MessageBox('Please try again and try checking if your username and password are correct', 'Warning', g.wx.OK | g.wx.ICON_WARNING)
 		
--- libs/main.py
+++ libs/main.py
@@ -2,6 +2,7 @@ from . import globals as g
 #the file where the bulk of the action occurs
 app=g.wx.App()
 APP_EXIT=1
+APP_LOGOUT=2
 
 class frame(g.wx.Frame):
 	def __init__(self, *args, **kwargs):
@@ -12,8 +13,11 @@ class frame(g.wx.Frame):
 		toolbar=g.wx.MenuBar()
 		filemenu=g.wx.Menu()
 		quitoption=g.wx.MenuItem(filemenu, APP_EXIT, '&Quit')
+		logout=g.wx.MenuItem(filemenu, APP_LOGOUT, '&log out')
 		filemenu.Append(quitoption)
+		filemenu.Append(logout)
 		self.Bind(g.wx.EVT_MENU, self.On_Quit, id=APP_EXIT)
+		self.Bind(g.wx.EVT_MENU, self.On_Logout, id=APP_LOGOUT)
 		toolbar.Append(filemenu, '&File')
 		self.SetMenuBar(toolbar)
 		
@@ -26,6 +30,13 @@ class frame(g.wx.Frame):
 	def On_Quit(self, e):
 		self.Close()
 	
+	def On_Logout(self,e):
+		with open('config/user/usr','r+') as f:
+			user=f.read()
+		password = g.keyring.get_password('AO3', user)
+		g.keyring.delete_password('AO3', user, password)
+		g.session=None
+
 def main():
 	g.frame=frame(None)
 	app.MainLoop()
